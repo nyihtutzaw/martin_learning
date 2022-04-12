6d3987dec0ab6952856f9d0dc2d36748229904ca
Aung Ko Man <aungkoman@gmail.com>
2022-04-12 13:14:44 +0630

    baseUrl head to development server

--- lib/network/api_service.dart
+++ lib/network/api_service.dart
@@ -6,8 +6,10 @@ class ApiService {
   static Dio getApiHandler(String token) {
     BaseOptions options = new BaseOptions(
       // baseUrl: "http://54.255.18.148/marthin/public/api/v1/",
-      baseUrl: "http://52.47.68.101/marthin/public/api/v1/",
+      // baseUrl: "http://52.47.68.101/marthin/public/api/v1/",
+      baseUrl: "http://52.47.68.101/learning-app-backend/public/api/v1/",
       //baseUrl: "http://yourlink.mar-thin.com/api/v1/",
+      // http://192.168.98.174/
     );
 
     if (token != null) options.headers["Authorization"] = "Bearer " + token;
