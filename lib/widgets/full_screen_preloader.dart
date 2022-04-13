5d47e24d8e8b2a49f51553e34182994f379faabe
Aung Ko Man <aungkoman@gmail.com>
2022-04-13 15:57:46 +0630

    loding indicator center placed

--- lib/widgets/full_screen_preloader.dart
+++ lib/widgets/full_screen_preloader.dart
@@ -5,7 +5,8 @@ class FullScreenPreloader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        // padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5),
+        //padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5),
+        padding: EdgeInsets.all(8.0),
         child: CircularProgressIndicator(),
       ),
     );
