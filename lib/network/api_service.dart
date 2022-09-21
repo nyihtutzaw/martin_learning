eea5d885727db5f8828e0227ff29a0fec146863d
kaung-san-hein <kaungsanhein2019@gmail.com>
2022-09-21 15:36:39 +0630

    [Done]change server api

--- lib/network/api_service.dart
+++ lib/network/api_service.dart
@@ -6,8 +6,8 @@ class ApiService {
     BaseOptions options = BaseOptions(
       // baseUrl: "http://54.255.18.148/marthin/public/api/v1/",
       // baseUrl: "http://52.47.68.101/marthin/public/api/v1/",
-      baseUrl: "http://10.0.2.2:8000/api/v1/",
-      // baseUrl: "http://52.47.68.101/marthin_be/public/api/v1/",
+      // baseUrl: "http://10.0.2.2:8000/api/v1/",
+      baseUrl: "http://52.47.68.101/marthin_be/public/api/v1/",
       // baseUrl: "http://192.168.98.174/learning-app-backend/public/api/v1/",
       //baseUrl: "http://yourlink.mar-thin.com/api/v1/",
       // http://192.168.98.174/
